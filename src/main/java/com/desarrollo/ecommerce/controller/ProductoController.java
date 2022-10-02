ace082ba0e6cdd94ea4e432eb8230d824051a35d
Robert <robert-870519@hotmail.com>
2022-10-01 21:39:19 -0500

    Agregue funcionalidad para guardar productos desde el frontend, cree el formulario para el registro de nuevos productos a traves de la clase ProductoController

--- src/main/java/com/desarrollo/ecommerce/controller/ProductoController.java
+++ src/main/java/com/desarrollo/ecommerce/controller/ProductoController.java
@@ -1,8 +1,13 @@
 
 package com.desarrollo.ecommerce.controller;
 
+import com.desarrollo.ecommerce.model.Producto;
+import com.desarrollo.ecommerce.model.Usuario;
+import com.desarrollo.ecommerce.service.ProductoService;
+import org.slf4j.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -13,8 +18,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+    
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+    
+    private ProductoService productoService;
+    
     @GetMapping("")
     public String show(){
         return "productos/itemproduct";
+    }
+    
+    @GetMapping("/createproduct")
+    public String create(){
+        return "productos/createproduct";
+    }
+    
+    @PostMapping("/save")
+    public String save(Producto producto){
+        LOGGER.info("Este es el objeto producto {}", producto);
+        Usuario u = new Usuario(1, "", "", "", "", "", "", "");
+        producto.setUsuario(u);
+        productoService.save(producto);
+        return "redirect:/productos";
     }
 }
