0c327b8f534f8bb70b2f19d74522c284fe744466
Robert <robert-870519@hotmail.com>
2022-10-04 01:14:15 -0500

    Creación de la página detalle producto y mapeo de la url desde el Controlador Home

--- src/main/java/com/desarrollo/ecommerce/HomeController.java
+++ src/main/java/com/desarrollo/ecommerce/HomeController.java
@@ -2,10 +2,12 @@
 package com.desarrollo.ecommerce;
 
 import com.desarrollo.ecommerce.service.ProductoService;
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -17,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
     
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
+    
     @Autowired 
     private ProductoService productoService; //Acceder a los productos agregados desde el admin
     
@@ -24,5 +28,11 @@ public class HomeController {
     public String home(Model model){
         model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
+    }
+    
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id){ //mostramos el producto por el id
+        log.info("Id producto enviado como parametro {}", id);
+        return "usuario/productohome";
     }
 }
