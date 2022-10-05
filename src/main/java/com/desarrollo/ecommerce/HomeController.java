1bc86723acb8539727e71394980e9c6b4b49d6ab
Robert <robert-870519@hotmail.com>
2022-10-05 00:34:58 -0500

    creación de la plantilla carrito de compras, se añadió el mapeo en home controller

--- src/main/java/com/desarrollo/ecommerce/HomeController.java
+++ src/main/java/com/desarrollo/ecommerce/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -42,5 +43,10 @@ public class HomeController {
         model.addAttribute("producto", producto);
         
         return "usuario/productohome";
+    }
+    
+    @PostMapping("/carrito")
+    public String addCart(){
+        return "usuario/carrito";
     }
 }
