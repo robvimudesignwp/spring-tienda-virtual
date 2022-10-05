e57abd47d7fd4577a6e98fb73222d75128a0885d
Robert <robert-870519@hotmail.com>
2022-10-05 18:18:58 -0500

    creamos acceso a la cantidad y detalles del producto para mostrarlo en la vista carrito.html

--- src/main/java/com/desarrollo/ecommerce/HomeController.java
+++ src/main/java/com/desarrollo/ecommerce/HomeController.java
@@ -1,8 +1,12 @@
 
 package com.desarrollo.ecommerce;
 
+import com.desarrollo.ecommerce.model.DetalleOrden;
+import com.desarrollo.ecommerce.model.Orden;
 import com.desarrollo.ecommerce.model.Producto;
 import com.desarrollo.ecommerce.service.ProductoService;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -26,6 +31,10 @@ public class HomeController {
     
     @Autowired 
     private ProductoService productoService; //Acceder a los productos agregados desde el admin
+    
+    List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();//almacenamos los detalles de la orden de compra
+    
+    Orden orden = new Orden();
     
     @GetMapping("")
     public String home(Model model){
@@ -46,7 +55,16 @@ public class HomeController {
     }
     
     @PostMapping("/carrito")
-    public String addCart(){
+    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad ){
+        
+        DetalleOrden detalleOrden = new DetalleOrden();
+        Producto producto = new Producto();
+        double sumaTotal = 0; //la variable siempre iniciará en 0
+        
+        Optional<Producto> optionalProducto = productoService.get(id);
+        log.info("Producto añadido: {}", optionalProducto.get());
+        log.info("Cantidad: {}", cantidad);
+        
         return "usuario/carrito";
     }
 }
